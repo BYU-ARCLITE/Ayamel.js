0dee9036619d50a5a58c0933b85a3a2ea5a6b61e
Josh Monson <camman3d@gmail.com>
2013-03-13 11:01:04 -0600

    Added unit test stuff. Removed captions stuff.

---
+++ test/tests.js
@@ -0,0 +1,6 @@
+/*
+ * Check that all the different parts of the Ayamel global object have been created.
+ */
+test("UniformAspectRatio instantiated", function () {
+    ok(Ayamel.UniformAspectRatio, "UniformAspectRatio should have been created.")
+});
