7841c16d35f1e606852d4c89359711c72a214eca
Logan Kearsley <chronosurfer@gmail.com>
2013-05-08 12:17:30 -0600

    Fixed index error in processTime.
    Also made it a lot more concise, and fixed formatting (converted tabs to
    spaces).

--- js/player2/js/MediaPlayer.js
+++ js/player2/js/MediaPlayer.js
@@ -4,31 +4,23 @@
     var template = '<div class="mediaPlayer"></div>';
 
     function processTime(time) {
-        try {
-            if (typeof time === "number")
-                return time;
-            var parts = time.split(":");
-            if (parts.length === 3)
-                return Number(parts[0]) * 3600 + Number(parts[1]) * 60 + Number(parts[0]);
-            if (parts.length === 2)
-                return Number(parts[0] * 60) + Number(parts[1]);
-            if (parts.length === 1)
-                return Number(parts[0]);
-        } catch (e) {
-            console.log(e);
+        if (typeof time === "number") {
+            return time;
         }
-        return 0;
+        return time.split(":").reduce(function(last, next){
+            return last * 60 + (+part||0);
+        }, 0);
     }
 
     function MediaPlayer(args) {
         var _this = this, i,
-			pluginModule, plugin = null,
-			resource = args.resource,
-			registeredPlugins = Ayamel.mediaPlugins[resource.type] || {},
-			pluginPriority = Ayamel.prioritizedPlugins[resource.type] || [],
-			$element = $(template),
-			startTime = processTime(args.startTime || 0),
-			endTime = processTime(args.endTime || -1);
+            pluginModule, plugin = null,
+            resource = args.resource,
+            registeredPlugins = Ayamel.mediaPlugins[resource.type] || {},
+            pluginPriority = Ayamel.prioritizedPlugins[resource.type] || [],
+            $element = $(template),
+            startTime = processTime(args.startTime || 0),
+            endTime = processTime(args.endTime || -1);
 
         // Set up the element
         this.$element = $element;
@@ -37,34 +29,34 @@
 
         // Load the resource
         if (!pluginPriority.length) {
-			pluginPriority = Object.keys(registeredPlugins);
-		} else {
-			pluginPriority = pluginPriority.filter(function(name){
-				return registeredPlugins.hasOwnProperty(name);
-			});
-			[].push.apply(pluginPriority,Object.keys(registeredPlugins).filter(function(name){
-				return pluginPriority.indexOf(name) === -1;
-			}));
-		}
-		for(i = 0; plugin === null && (pluginModule = registeredPlugins[pluginPriority[i]]); i++){
-			if (!pluginModule.supports(args.resource)) { continue; }
-			plugin = pluginModule.install({
-				$holder: $element,
-				resource: resource,
-				aspectRatio: args.aspectRatio,
-				startTime: startTime,
-				endTime: endTime
-			});
-		}
+            pluginPriority = Object.keys(registeredPlugins);
+        } else {
+            pluginPriority = pluginPriority.filter(function(name){
+                return registeredPlugins.hasOwnProperty(name);
+            });
+            [].push.apply(pluginPriority,Object.keys(registeredPlugins).filter(function(name){
+                return pluginPriority.indexOf(name) === -1;
+            }));
+        }
+        for(i = 0; plugin === null && (pluginModule = registeredPlugins[pluginPriority[i]]); i++){
+            if (!pluginModule.supports(args.resource)) { continue; }
+            plugin = pluginModule.install({
+                $holder: $element,
+                resource: resource,
+                aspectRatio: args.aspectRatio,
+                startTime: startTime,
+                endTime: endTime
+            });
+        }
 
         // There needs to be a place for captions
         if (plugin === null) {
-			throw new Error("Could Not Find Resource Representation Compatible With Your Machine & Browser");
-		}
-		
-		this.plugin = plugin;
+            throw new Error("Could Not Find Resource Representation Compatible With Your Machine & Browser");
+        }
+
+        this.plugin = plugin;
         this.$captionsElement = plugin.$captionsElement;
-		this.captionsElement = plugin.captionsElement;
+        this.captionsElement = plugin.captionsElement;
 
         Object.defineProperties(this, {
             duration: {
@@ -86,7 +78,7 @@
                     if (this.plugin) {
                         return this.plugin.currentTime = time;
                     }
-					return 0;
+                    return 0;
                 }
             },
             muted: {
@@ -100,7 +92,7 @@
                     if (this.plugin) {
                         return this.plugin.muted = muted;
                     }
-					return false;
+                    return false;
                 }
             },
             paused: {
@@ -122,7 +114,7 @@
                     if (this.plugin) {
                         return this.plugin.playbackRate = playbackRate;
                     }
-					return 1;
+                    return 1;
                 }
             },
             readyState: {
@@ -144,7 +136,7 @@
                     if (this.plugin) {
                         return this.plugin.volume = volume;
                     }
-					return 1;
+                    return 1;
                 }
             }
         });
