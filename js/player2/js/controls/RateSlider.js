244ed437ef3e9222ff8c185ddcb024361d807046
Logan Kearsley <chronosurfer@gmail.com>
2013-05-10 17:33:23 -0600

    Extracted common slider widget.

--- js/player2/js/controls/RateSlider.js
+++ js/player2/js/controls/RateSlider.js
@@ -4,89 +4,55 @@
     var template =
         '<div class="rate">\
             <div class="speedIcon"></div>\
-            <div class="volumeSlider">\
-                <div class="volumeLevelPad"></div>\
-                <div class="volumeLevel"></div>\
-                <div class="volumeKnob"></div>\
-                <div class="volumeLevelPad"></div>\
-            </div>\
         </div>';
 
     function dispatchRate(element,rate) {
         var newEvent = document.createEvent("HTMLEvents");
-		newEvent.initEvent("ratechange", true, true);
-		newEvent.playbackRate = rate;
-		element.dispatchEvent(newEvent);
+        newEvent.initEvent("ratechange", true, true);
+        newEvent.playbackRate = rate;
+        element.dispatchEvent(newEvent);
     }
 
     function RateSlider(args) {
         var _this = this,
-			playing = false,
-			$element = $(template),
-			element = $element[0],
-			$level = $element.find(".volumeLevel"),
-			muted = false,
-			moving = false,
-			rate = 1,
-			levelPosition;
-			
-        this.$element = $element;
-		this.element = element;
-        args.$holder.append($element);
-			
-        $element.children(".volumeSlider").mousedown(function (event) {
-			var width, newEvent;
-            if (!moving) {
-                levelPosition = $level.offset().left;
-                moving = true;
-
-                width = Math.min(Math.max(event.pageX - levelPosition, 0), 100);
-                $level.width(width);
-				
-				rate = width / 100;
-				dispatchRate(element,rate);
-            }
-        });
-        $("body")
-            .mousemove(function (event) {
-				var width;
-                if (moving) {
-                    width = Math.min(Math.max(event.pageX - levelPosition, 0), 100);
-                    $level.width(width);
-
-					rate = width / 100;
-					dispatchRate(element,rate);
-                }
-            }).mouseup(function (event) {
-                if (moving) {
-                    moving = false;
-                    event.stopPropagation();
-
-					rate = $level.width() / 100;
-					dispatchRate(element,rate);
-                }
+            rate = 1,
+            $element = $(template),
+            element = $element[0],
+            slider = new Ayamel.controls.slider({
+                $holder: $element,
+                level: 100
             });
 
+        this.$element = $element;
+        this.element = element;
+        args.$holder.append($element);
+
+        slider.addEventListener('levelchange',function(percent){
+            rate = percent / 100;
+            slider.level = percent;
+            dispatchRate(element,rate);
+        },false);
+
         // Allow resetting
-        $element.children(".muteIcon").click(function () {
-			rate = 1;
-			$level.width(100);
-			dispatchRate(element,1);
+        $element.children(".speedIcon").click(function () {
+            rate = 1;
+            slider.level = 100;
+            dispatchRate(element,1);
         });
-		
+
         // Be able to set the rate attribute
         Object.defineProperties(this, {
-			rate: {
-				enumerable: true,
-				get: function () {
-					return rate;
-				},
-				set: function (value) {
-					rate = +value||0;
-					$level.width(rate*100);
-					return rate;
-				}
-			}
+            rate: {
+                enumerable: true,
+                get: function () {
+                    return rate;
+                },
+                set: function (value) {
+                    rate = +value||0;
+                    slider.level = rate*100;
+                    return rate;
+                }
+            }
         });
     }
 
--- js/player2/js/controls/VolumeSlider.js
+++ js/player2/js/controls/VolumeSlider.js
@@ -9,110 +9,77 @@
     "use strict";
 
     var template =
-        '<div class="volume">' +
-            '<div class="muteIcon"></div>' +
-            '<div class="volumeSlider">' +
-                '<div class="volumeLevelPad"></div>' +
-                '<div class="volumeLevel"></div>' +
-                '<div class="volumeKnob"></div>' +
-                '<div class="volumeLevelPad"></div>' +
-            '</div>' +
-        '</div>';
+        '<div class="volume">\
+            <div class="muteIcon"></div>\
+        </div>';
 
     function dispatchVolume(element,volume) {
         var newEvent = document.createEvent("HTMLEvents");
-		newEvent.initEvent("volumechange", true, true);
-		newEvent.volume = volume;
-		element.dispatchEvent(newEvent);
+        newEvent.initEvent("volumechange", true, true);
+        newEvent.volume = volume;
+        element.dispatchEvent(newEvent);
     }
 
     function VolumeSlider(args) {
         var _this = this,
-			playing = false,
-			$element = $(template),
-			element = $element[0],
-			$level = $element.find(".volumeLevel"),
-			muted = false,
-			moving = false,
-			volume = 1,
-			levelPosition;
-			
-        this.$element = $element;
-		this.element = element;
-        args.$holder.append($element);
-			
-        $element.children(".volumeSlider").mousedown(function (event) {
-			var width, newEvent;
-            if (!moving) {
-                levelPosition = $level.offset().left;
-                moving = true;
-
-                width = Math.min(Math.max(event.pageX - levelPosition, 0), 100);
-                $level.width(width);
-				
-				volume = width / 100;
-				dispatchVolume(element,volume);
-            }
-        });
-        $("body")
-            .mousemove(function (event) {
-				var width;
-                if (moving) {
-                    width = Math.min(Math.max(event.pageX - levelPosition, 0), 100);
-                    $level.width(width);
-
-					volume = width / 100;
-					dispatchVolume(element,volume);
-                }
-            }).mouseup(function (event) {
-                if (moving) {
-                    moving = false;
-                    event.stopPropagation();
-
-					volume = $level.width() / 100;
-					dispatchVolume(element,volume);
-                }
+            volume = 1,
+            muted = false,
+            $element = $(template),
+            element = $element[0],
+            slider = new Ayamel.controls.slider({
+                $holder: $element,
+                level: 100
             });
+
+        this.$element = $element;
+        this.element = element;
+        args.$holder.append($element);
+
+        slider.addEventListener('levelchange',function(percent){
+            volume = percent / 100;
+            slider.level = percent;
+            dispatchVolume(element,volume);
+        },false);
 
         // Allow muting
         $element.children(".muteIcon").click(function () {
             var newEvent = document.createEvent("HTMLEvents");
             if (muted) {
                 muted = false;
-                $element.removeClass("muted");
+                element.classList.remove("muted");
                 newEvent.initEvent("unmute", true, true);
             } else {
                 muted = true;
-                $element.addClass("muted");
+                element.classList.add("muted");
                 newEvent.initEvent("mute", true, true);
             }
             element.dispatchEvent(newEvent);
         });
-		
+
         // Be able to set the muted & volume attributes
         Object.defineProperties(this, {
-			muted: {
-				enumerable: true,
-				get: function () {
-					return muted;
-				},
-				set: function (value) {
-					muted = !!value;
-					$element[muted?'addClass':'removeClass']("muted");
-					return muted;
-				}
-			},
-			volume: {
-				enumerable: true,
-				get: function () {
-					return volume;
-				},
-				set: function (value) {
-					volume = +value||0;
-					$level.width(volume*100);
-					return volume;
-				}
-			}
+            muted: {
+                enumerable: true,
+                get: function () {
+                    return muted;
+                },
+                set: function (value) {
+                    muted = !!value;
+                    element.classList[muted?'add':'remove']("muted");
+                    return muted;
+                }
+            },
+            volume: {
+                enumerable: true,
+                get: function () {
+                    return volume;
+                },
+                set: function (value) {
+                    volume = +value||0;
+                    $level.width(volume*100);
+                    return volume;
+                }
+            }
         });
     }
 
