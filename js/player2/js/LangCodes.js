a5f1e639f458d484ac0d1a80ae8023365c1a2789
Logan Kearsley <chronosurfer@gmail.com>
2013-06-11 17:44:20 -0600

    Added Part 1 - Part 3 lang code mapping

--- js/player2/js/LangCodes.js
+++ js/player2/js/LangCodes.js
@@ -1,5 +1,6 @@
 (function(Ayamel){
     "use strict";
+	//TODO: Dynamically load from the most up-to-date copy of the iso639 registry
 	var langCodes = {
 	aaa:{part2b:"",part2t:"",part1:"",scope:"I",type:"L",name:"Ghotuo"},
 	aab:{part2b:"",part2t:"",part1:"",scope:"I",type:"L",name:"Alumu-Tesu"},
@@ -7875,11 +7876,42 @@
 	zyp:{part2b:"",part2t:"",part1:"",scope:"I",type:"L",name:"Zyphe Chin"},
 	zza:{part2b:"zza",part2t:"zza",part1:"",scope:"M",type:"L",name:"Zaza"},
 	zzj:{part2b:"",part2t:"",part1:"",scope:"I",type:"L",name:"Zuojiang Zhuang"}
+	},
+	p1map = {
+	aa:"aar",ab:"abk",af:"afr",ak:"aka",am:"amh",ar:"ara",an:"arg",as:"asm",
+	av:"ava",ae:"ave",ay:"aym",az:"aze",ba:"bak",bm:"bam",be:"bel",bn:"ben",
+	bi:"bis",bo:"bod",bs:"bos",br:"bre",bg:"bul",ca:"cat",cs:"ces",ch:"cha",
+	ce:"che",cu:"chu",cv:"chv",kw:"cor",co:"cos",cr:"cre",cy:"cym",da:"dan",
+	de:"deu",dv:"div",dz:"dzo",el:"ell",en:"eng",eo:"epo",et:"est",eu:"eus",
+	ee:"ewe",fo:"fao",fa:"fas",fj:"fij",fi:"fin",fr:"fra",fy:"fry",ff:"ful",
+	gd:"gla",ga:"gle",gl:"glg",gv:"glv",gn:"grn",gu:"guj",ht:"hat",ha:"hau",
+	sh:"hbs",he:"heb",hz:"her",hi:"hin",ho:"hmo",hr:"hrv",hu:"hun",hy:"hye",
+	ig:"ibo",io:"ido",ii:"iii",iu:"iku",ie:"ile",ia:"ina",id:"ind",ik:"ipk",
+	is:"isl",it:"ita",jv:"jav",ja:"jpn",kl:"kal",kn:"kan",ks:"kas",ka:"kat",
+	kr:"kau",kk:"kaz",km:"khm",ki:"kik",rw:"kin",ky:"kir",kv:"kom",kg:"kon",
+	ko:"kor",kj:"kua",ku:"kur",lo:"lao",la:"lat",lv:"lav",li:"lim",ln:"lin",
+	lt:"lit",lb:"ltz",lu:"lub",lg:"lug",mh:"mah",ml:"mal",mr:"mar",mk:"mkd",
+	mg:"mlg",mt:"mlt",mn:"mon",mi:"mri",ms:"msa",my:"mya",na:"nau",nv:"nav",
+	nr:"nbl",nd:"nde",ng:"ndo",ne:"nep",nl:"nld",nn:"nno",nb:"nob",no:"nor",
+	ny:"nya",oc:"oci",oj:"oji",or:"ori",om:"orm",os:"oss",pa:"pan",pi:"pli",
+	pl:"pol",pt:"por",ps:"pus",qu:"que",rm:"roh",ro:"ron",rn:"run",ru:"rus",
+	sg:"sag",sa:"san",si:"sin",sk:"slk",sl:"slv",se:"sme",sm:"smo",sn:"sna",
+	sd:"snd",so:"som",st:"sot",es:"spa",sq:"sqi",sc:"srd",sr:"srp",ss:"ssw",
+	su:"sun",sw:"swa",sv:"swe",ty:"tah",ta:"tam",tt:"tat",te:"tel",tg:"tgk",
+	tl:"tgl",th:"tha",ti:"tir",to:"ton",tn:"tsn",ts:"tso",tk:"tuk",tr:"tur",
+	tw:"twi",ug:"uig",uk:"ukr",ur:"urd",uz:"uzb",ve:"ven",vi:"vie",vo:"vol",
+	wa:"wln",wo:"wol",xh:"xho",yi:"yid",yo:"yor",za:"zha",zh:"zho",zu:"zul"
 	};
 	
 	Ayamel.utils.langCodes = langCodes;
 	Ayamel.utils.getLangName = function(ncode,lcode){
 		//should do a lookup on a localization server
 		return langCodes[ncode].name;
+	};
+	Ayamel.utils.upgradeLangCode = function(p1code){
+		if(!p1map.hasOwnProperty(p1code)){
+			throw new Error("Invalid Part 1 Language Code");
+		}
+		return p1map[p1code];
 	};
 }(Ayamel));
