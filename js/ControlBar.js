3c7d0cf1ba4e7222afe643d954d5982331d5a7c1
Logan Kearsley <chronosurfer@gmail.com>
2013-07-02 14:22:35 -0600

    Removed Cruft

--- js/ControlBar.js
+++ js/ControlBar.js
@@ -1,150 +1,99 @@
-/**
- * ControlBar.js
- * This is an alternate control bar.
- * Instead of MediaControls.js include
- * <ol>
- *     <li>ProgressBar.js</li>
- *     <li>ControlBar.js</li>
- * </ol>
- *
- * Requires
- *  - JQuery
- *  - ProgressBar.js
- * Used by
- *  - MediaController.js
- */
-var ControlBar = (function () {
+(function(Ayamel) {
     "use strict";
 
-    // Templates for the control bar and component holder
-    var template = '<div class="controlBar"></div>',
-        componentHolderTemplate = '<div class="components"></div>';
+    var wtemplate =
+        '<div class="widgets">\
+            <div class="left"></div>\
+            <div class="right"></div>\
+        </div>';
 
-    // A helper function to help find and work with components
-    function getComponent(components, name) {
-        var validComponents = components.filter(function (component) {
-            return component.name === name;
+    function addComponent($controls, component) {
+        var constructor = Ayamel.controls[component];
+        if(typeof constructor !== 'function'){ return; }
+        this.components[component] = new constructor({
+            parent: this,
+            $holder: $controls
         });
-        if (validComponents.length > 0) {
-            return validComponents[0];
-        }
-        return null;
     }
 
-    /**
-     * The Control Bar object.
-     * @constructor
-     */
-    function ControlBar(attributes) {
+    function ControlBar(args) {
         var _this = this,
+            controlLists = args.components || {left:["play", "volume", "captions"], right:["rate", "fullScreen", "timeCode"]},
+            components = {}, progressBar, timeCode,
+            currentTime = 0, duration = 0,
+            element = document.createElement('div'),
+            $element = $(element);
 
-            // The progress bar object
-            progressBar = new ProgressBar(this),
+        element.className = "controlBar";
 
-            // If no components are defined, then use a default set
-            componentNames = attributes.componentNames || ["play", "volume", "fullScreen"],
-
-            // Create the element
-            $element = $(template),
-
-            // Create the component holder
-            $componentHolder = $(componentHolderTemplate);
-
-        // Turn the list of names into actual components
-        this.components = componentNames.map(function (name) {
-            return ControlBarComponents[name](_this, attributes);
+        // Create the ProgressBar
+        progressBar = new Ayamel.classes.ProgressBar({
+            $holder: $element
         });
 
-        // Add the progress bar
-        $element.append(progressBar.element);
+        $element.append(wtemplate);
 
-        // Add the components
-        $element.append($componentHolder);
-        this.components.forEach(function (component) {
-            $componentHolder.append(component.element);
-        });
+        this.element = element;
+        this.$element = $element;
+        args.$holder.append($element);
 
-        //Define properties for this object
+        //set default values
+        this.volume = 0;
+        this.muted = false;
+        this.playbackRate = 0;
+        this.playing = false;
+        this.fullScreen = false;
+
+        // Create the control bar components
+        this.components = components;
+
+        if(controlLists.left instanceof Array){
+            controlLists.left.forEach(addComponent.bind(this,$element.find(".left")));
+        }
+        if(controlLists.right instanceof Array){
+            controlLists.right.forEach(addComponent.bind(this,$element.find(".right")));
+        }
+
+        timeCode = components.timeCode || {};
         Object.defineProperties(this, {
+            currentTime: {
+                enumerable: true,
+                set: function (value) {
+                    currentTime = Math.min(+value||0,duration);
+                    progressBar.progress = currentTime / duration;
+                    timeCode.currentTime = currentTime;
+                    return currentTime;
+                },
+                get: function () {
+                    return currentTime;
+                }
+            },
             duration: {
+                enumerable: true,
                 set: function (value) {
-                    progressBar.duration = value;
+                    duration = +value||0;
+                    timeCode.duration = duration;
+                    if(currentTime > duration) {
+                        currentTime = duration;
+                        progressBar.progress = 1;
+                        timeCode.currentTime = duration;
+                    }
+                    return duration;
                 },
                 get: function () {
-                    return progressBar.duration;
-                }
-            },
-            element: {
-                get: function () {
-                    return $element.get(0);
-                }
-            },
-            muted: {
-                set: function (value) {
-                    var volume = getComponent(_this.components, "volume");
-                    if (volume) {
-                        volume.muted = value;
-                    }
-                },
-                get: function () {
-                    var volume = getComponent(_this.components, "volume");
-                    if (volume) {
-                        return volume.muted;
-                    }
-                    return false;
-                }
-            },
-            playing: {
-                get: function () {
-                    var play = getComponent(_this.components, "play");
-                    if (play) {
-                        return play.playing;
-                    }
-                    return false;
-                },
-                set: function (value) {
-                    var play = getComponent(_this.components, "play");
-                    if (play) {
-                        play.playing = value;
-                    }
-                }
-            },
-            progress: {
-                set: function (value) {
-                    progressBar.progress = value;
-                },
-                get: function () {
-                    return progressBar.progress;
-                }
-            },
-            volume: {
-                set: function (value) {
-                    var volume = getComponent(_this.components, "volume");
-                    if (volume) {
-                        volume.volume = value;
-                    }
-                },
-                get: function () {
-                    var volume = getComponent(_this.components, "volume");
-                    if (volume) {
-                        return volume.volume;
-                    }
-                    return 100;
+                    return duration;
                 }
             }
         });
     }
 
-    ControlBar.prototype.addEventListener = function (eventName, callback) {
-        this.element.addEventListener(eventName, callback, false);
-    };
-    ControlBar.prototype.removeEventListener = function (eventName, callback) {
-        this.element.removeEventListener(eventName, callback, false);
-    };
-    ControlBar.prototype.getComponent = function(name) {
-        return getComponent(this.components, name);
+    ControlBar.prototype.addEventListener = function(event, callback, capture) {
+        this.element.addEventListener(event, callback, !!capture);
     };
 
-    return ControlBar;
+    ControlBar.prototype.removeEventListener = function(event, callback, capture) {
+        this.element.removeEventListener(event, callback, !!capture);
+    };
 
-}());
+    Ayamel.classes.ControlBar = ControlBar;
+}(Ayamel));
