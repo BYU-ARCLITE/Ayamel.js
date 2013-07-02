99dc555ccf74bdfb0f4e6d69a3338569e3ab3725
Logan Kearsley <chronosurfer@gmail.com>
2013-07-02 14:22:35 -0600

    Removed Cruft

--- js/ProgressBar.js
+++ js/ProgressBar.js
@@ -1,122 +1,113 @@
 /**
- * ProgressBar.js
- * The progress bar is part of the media controls and represents the progress of the media being played.
- *
- * Requires:
- *  - JQuery
- * Used by:
- *  - MediaControls2.js
+ * Created with IntelliJ IDEA.
+ * User: camman3d
+ * Date: 4/30/13
+ * Time: 2:12 PM
+ * To change this template use File | Settings | File Templates.
  */
-var ProgressBar = (function () {
+(function(Ayamel, global) {
     "use strict";
 
     var template =
-        '<div class="timeBar">' +
-        '    <div class="progressBar"></div><div class="progressKnob"></div>' +
-        '</div>';
+        '<div class="progressBar">\
+            <div class="progressLevel">\
+                <div class="progressKnob"></div>\
+            </div>\
+        </div>';
 
-    /**
-     * The Progress Bar object. This is used to keep track of the time progress.
-     * This contains the following properties:
-     * <ul>
-     *     <li>duration (get/set) - The length, in seconds, of the media being represented.</li>
-     *     <li>element (get) - The HTML element which is depicting the progress</li>
-     *     <li>progress (get/set) - The current position, in seconds, of the media.</li>
-     * </ul>
-     * @constructor
-     */
-    function ProgressBar(controlBar) {
-        var duration = 0,
-            progress = 0,
-            $element = $(template), // Create the HTML element
-            $knob = $element.children(".progressKnob"),
+    function ProgressBar(args) {
+        var  _this = this,
+            max = isNaN(+args.max)?1:(+args.max),
+            min = +args.min||0,
+            scale = 100/(max-min),
+            value = Math.min(+args.progress||0,max),
+            $element = $(template),
+            element = $element[0],
+            level = $element.find(".progressLevel")[0],
+            left = 0, lastX = 0,
             moving = false;
 
-        // Add click functionality
-        $knob.bind("touchstart mousedown", function (event) {
+        this.$element = $element;
+        this.element = element;
+        args.$holder.append($element);
+
+        level.style.width = (value-min)*scale+"%";
+
+        function pxToValue(px){
+            var pmax = parseInt(global.getComputedStyle(element,null).getPropertyValue('width'),10),
+                pc = Math.min(Math.max(px,0),pmax)/pmax;
+            return pc*(max-min)+min;
+        }
+
+        element.addEventListener(Ayamel.utils.mobile.isMobile ? "touchstart" : "mousedown", function (event) {
+            var newEvent;
+            if (moving) { return; }
+            left = $element.offset().left + 10;
             moving = true;
+
+            lastX = event.pageX;
+            value = pxToValue(lastX - left);
+            level.style.width = (value-min)*scale+"%";
+
+            newEvent = document.createEvent("HTMLEvents");
+            newEvent.initEvent("scrubstart", true, true);
+            newEvent.progress = value;
+            element.dispatchEvent(newEvent);
             event.stopPropagation();
-        });
+        }, false);
 
-        $(document)
-            .bind("touchmove mousemove", function (event) {
-                if (moving) {
-                    var width = event.pageX - $element.offset().left;
-                    $element.children(".progressBar").width(width);
-                }
-            }).bind("touchend mouseup", function (event) {
-                if (moving) {
-                    moving = false;
+        document.body.addEventListener(Ayamel.utils.mobile.isMobile ? "touchmove" : "mousemove", function (event) {
+            var newEvent;
+            if (!moving) { return; }
 
-                    var percentage = $element.children(".progressBar").width() / $element.width(),
-                        newEvent;
+            lastX = event.pageX;
+            value = pxToValue(lastX - left);
+            level.style.width = (value-min)*scale+"%";
 
-                    // Create a new event and dispatch it through the control bar
-                    newEvent = document.createEvent('HTMLEvents');
-                    newEvent.initEvent('progressupdate',true,true);
-                    newEvent.progress = progress = percentage * duration;
-                    controlBar.element.dispatchEvent(newEvent);
+            newEvent = document.createEvent("HTMLEvents");
+            newEvent.initEvent("scrubupdate", true, true);
+            newEvent.progress = value;
+            element.dispatchEvent(newEvent);
+            event.stopPropagation();
+        }, false);
 
-                    console.log(progress);
+        document.body.addEventListener(Ayamel.utils.mobile.isMobile ? "touchend" : "mouseup", function (event) {
+            var newEvent;
+            if (!moving) { return; }
+            moving = false;
 
-                    // Prevent the parent element from handling the event
-                    event.stopPropagation();
-                }
-            });
+            value = pxToValue((Ayamel.utils.mobile.isMobile ? lastX : event.pageX) - left);
+            level.style.width = (value-min)*scale+"%";
 
-        $element.bind("touchend mouseup", function (event) {
-            console.log("click");
-            var mouseX = event.offsetX || event.layerX,
-                percentage,
-                newEvent;
-
-            if (event.button !== 0) {
-                return;
-            }
-
-            // Figure out the new volume level
-            percentage = mouseX / $(this).width();
-
-            // Create a new event and dispatch it through the control bar
-            newEvent = document.createEvent('HTMLEvents');
-            newEvent.initEvent('progressupdate',true,true);
-            newEvent.progress = progress = percentage * duration;
-            controlBar.element.dispatchEvent(newEvent);
-        });
+            newEvent = document.createEvent("HTMLEvents");
+            newEvent.initEvent("scrubend", true, true);
+            newEvent.progress = value;
+            element.dispatchEvent(newEvent);
+            event.stopPropagation();
+        }, false);
 
         Object.defineProperties(this, {
-            duration: {
-                get: function () {
-                    return duration;
-                },
-                set: function (value) {
-                    duration = Number(value);
-                }
-            },
-            element: {
-                get: function () {
-                    return $element.get(0);
-                }
-            },
             progress: {
-                get: function () {
-                    return progress;
+                set: function(val){
+                    if (moving) { return value; }
+                    value = Math.max(Math.min(+val||0,max),min);
+                    level.style.width = (value-min)*scale+"%";
+                    return value;
                 },
-                set: function (value) {
-                    var width;
-
-                    // Set the progress bounding it between 0 and the duration
-                    progress = Math.max(Math.min(value, duration), 0);
-
-                    // Set the width of the progress bar if not dragging it
-                    if (!moving) {
-                        width = (duration ? progress / duration : 0) * 100;
-                        $element.find(".progressBar").width(width + "%");
-                    }
+                get: function(){
+                    return value;
                 }
             }
         });
     }
 
-    return ProgressBar;
-}());
+    ProgressBar.prototype.addEventListener = function(event, callback, capture) {
+        this.element.addEventListener(event, callback, !!capture);
+    };
+
+    ProgressBar.prototype.removeEventListener = function(event, callback, capture) {
+        this.element.removeEventListener(event, callback, !!capture);
+    };
+
+    Ayamel.classes.ProgressBar = ProgressBar;
+}(Ayamel, window));
