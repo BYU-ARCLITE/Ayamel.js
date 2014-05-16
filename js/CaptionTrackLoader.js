b5c4c50021afc29cc53329e39fa5f73eda025dbf
Logan Kearsley <chronosurfer@gmail.com>
2014-05-16 10:30:29 -0600

    Fixing errors in CaptionTrackLoader.

--- js/CaptionTrackLoader.js
+++ js/CaptionTrackLoader.js
@@ -8,25 +8,26 @@
 (function (Ayamel, TimedText) {
     "use strict";
 
-    Ayamel.utils.loadCaptionTrack = function (resource, successcb, errorcb) {
+    Ayamel.utils.loadCaptionTrack = function(resource, successcb, errorcb){
+		var supportedFiles, file;
 		if(!TimedText){ throw new Error("TimedText library not loaded."); }
-		if (resource instanceof TextTrack) {
-			callback(resource);
-		} else {
-			resource.content.files.forEach(function (file) {
-				if (TimedText.isSupported(file.mime)) {
-					TextTrack.get({
-						kind: (file.attributes && file.attributes.kind) || "subtitles",
-						label: resource.title || "Untitled",
-						lang: (resource.languages && resource.languages.iso639_3 && resource.languages.iso639_3[0]) || "eng",
-						url: file.downloadUri,
-						success: successcb,
-						error: errorcb
-					});
-				} else if(typeof errorcb === 'function') {
-					setTimeout(errorcb.bind(null, new Error("Unsupported MIME-Type")),0);
-				}
-			});
+		if(resource instanceof TextTrack){
+			setTimeout(successcb.bind(null,resource),0);
+		}else{
+			supportedFiles = resource.content.files.filter(function(file){ return TimedText.isSupported(file.mime); });
+			if(supportedFiles.length){
+				file = supportedFiles[0];
+				TextTrack.get({
+					kind: (file.attributes && file.attributes.kind) || "subtitles",
+					label: resource.title || "Untitled",
+					lang: (resource.languages && resource.languages.iso639_3 && resource.languages.iso639_3[0]) || "eng",
+					src: file.downloadUri,
+					success: successcb,
+					error: errorcb
+				});
+			}else if(typeof errorcb === 'function'){
+				setTimeout(errorcb.bind(null, new Error("Unsupported MIME-Type")),0);
+			}
 		}
 	};
 
