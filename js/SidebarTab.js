2e3419824b56e4358a7df3786e9e257d7323abd8
Logan Kearsley <chronosurfer@gmail.com>
2015-07-10 18:29:40 -0600

    Construct tabs inside the player.

--- js/SidebarTab.js
+++ js/SidebarTab.js
@@ -2,12 +2,11 @@
 	"use strict";
 
 	function renderHead(name) {
-        var result = document.createElement('li');
-        result.classList.add('tabHead');
-        result.classList.add('unselected');
-        var headDiv = document.createElement('div');
-        result.textContent = name;
-        return result;
+		var result = document.createElement('li');
+		result.classList.add('tabHead');
+		result.classList.add('unselected');
+		result.textContent = name;
+		return result;
 	}
 
 	function renderBody(name, content) {
@@ -19,34 +18,21 @@
 		return result;
 	}
 
-	function initCallbacks(tab, onClickTabInit) {
-		var callbacks = [];
-		if(typeof onClickTabInit === 'function') {
-			callbacks.push(onClickTabInit);
-		}
-		tab.clickTabCallbacks = callbacks;
-		tab.onClickTabInit = onClickTabInit;
-		function clickTabCallback() {
-			for(var i = 0; i < callbacks.length; i++) {
-				var callback = callbacks[i];
-				callback();
-			}
-		}
-		tab.head.addEventListener('click', clickTabCallback);
-	}
-	
 	function SidebarTab(args) {
 		var	that = this,
 			title = args.title,
-			onClickTab = args.onClickTab,
-			content = args.content;
+			content = args.content,
+			sidebar = args.sidebar;
 
 		this.title = title;
 		this.content = content;
 		this.head = renderHead(title);
 		this.body = renderBody(title, content);
 
-		initCallbacks(this, onClickTab);
+		this.head.addEventListener('click', function(){
+			sidebar.show();
+			sidebar.selectTab(that);
+		});
 	}
 
 	SidebarTab.prototype = {
@@ -61,16 +47,6 @@
 			this.body.classList.add('hidden');
 			this.head.classList.remove('selected');
 			this.head.classList.add('unselected');
-		},
-		clone: function() {
-			return new SidebarTab({
-				title: this.title,
-				content: this.content.cloneNode(true),
-				onClickTab: this.onClickTabInit
-			});
-		},
-		onClickTab: function(callback) {
-			this.clickTabCallbacks.push(callback);
 		}
 	}
 
